6c22b73736ba73860bc7c442649e6a2e8d89c20f
Djalil Moussa <djalileed@gmail.com>
2022-11-20 11:52:40 +0100

    feat(frontendi): adding new component to the App component

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,11 +1,29 @@
-import './App.css';
-import { Users } from './components/Users';
+/* eslint-disable */
+
+import React from 'react';
+import './styles/App.css';
+//import { Users } from './components/Users';
+import HomeNavBar from './components/HomeNavBar';
+import BoardEditor from './components/BoardEditor';
+import {
+	BrowserRouter,
+	Routes,
+	Route
+} from "react-router-dom";
+
+
 
 function App() {
 	return (
-		<div className="App">
-			<Users />
-		</div>
+		<>
+			<HomeNavBar />
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" />
+					<Route path="/pixelBoards/:id" element={<BoardEditor />} />
+				</Routes>
+			</BrowserRouter>
+		</>
 	);
 }
 
