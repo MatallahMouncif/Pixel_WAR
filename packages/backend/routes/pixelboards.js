b05165741d612e5d9732990a332450b80603d439
Quentin <quentin.bertramo@gmail.com>
2022-11-26 23:41:19 +0100

    feat(backend): pixelboard services

--- packages/backend/routes/pixelboards.js
+++ packages/backend/routes/pixelboards.js
@@ -26,4 +26,25 @@ router.get('/:id',
 		res.send(pixelboard);
 	});
 
+router.post('/create',
+	async (req, res) => {
+		const createdPixelboard = await pixelboardService.createPixelboard(req.body);
+
+		res.send(createdPixelboard);
+	});
+
+router.put('/:id/update',
+	async (req, res) => {
+		const updatedPixelboard = await pixelboardService.updatePixelboard(req.params.id, req.body);
+
+		res.send(updatedPixelboard);
+	});
+
+router.delete('/:id/delete',
+	async (req, res) => {
+		const deletedPixelboard = await pixelboardService.deletePixelboard(req.params.id);
+
+		res.send(deletedPixelboard);
+	});
+
 module.exports = router;
