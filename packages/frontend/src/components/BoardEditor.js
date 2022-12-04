1e1686b63424fae2db42b4c43891fdb0d6fb57b6
Djalil Moussa <djalileed@gmail.com>
2022-12-04 20:40:11 +0100

    feat(frontend) : Admin can delete pixelboard now

--- packages/frontend/src/components/BoardEditor.js
+++ packages/frontend/src/components/BoardEditor.js
@@ -1,4 +1,7 @@
-/* eslint-disable */
+/* eslint-disable react/jsx-no-bind */
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable prefer-const */
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect, useRef } from 'react';
 import '../styles/BoardEditor.css';
 import { useParams, useNavigate } from 'react-router-dom';
@@ -7,12 +10,10 @@ import 'bootstrap';
 import axios from 'axios';
 import { CirclePicker } from 'react-color';
 import { exportComponentAsPNG } from 'react-component-export-image';
-function BoardEditor(props) {
+
+function BoardEditor() {
 	const params = useParams();
 	const panelRef = useRef();
-	const [panelWidth, setPanelWidth] = useState(0);
-	const [panelHeight, setPanelHeight] = useState(0);
-	const [cellSideNumber, setCellSideNumber] = useState(0);
 	const [cellSize, setCellSize] = useState(20);
 	const [editMode, setEditMode] = useState(false);
 	let [lastCell, setLastCell] = useState(null);
@@ -20,56 +21,16 @@ function BoardEditor(props) {
 	let [selectedColor, setSelectedColor] = useState('#f44336');
 	let [pixels, setPixels] = useState(null);
 	let [board, setBoard] = useState({
-		title: "fetching", creation_date: "", override_available: "", creation_date: "", end_date: "", status: "",
-		user_delay: "",
-		author_id: "", visitor_allowed: []
+		title: 'fetching',
+		creation_date: '',
+		override_available: '',
+		end_date: '',
+		status: '',
+		user_delay: '',
+		author_id: '',
+		visitor_allowed: [],
 	});
-	const navigate = useNavigate();
-
-
-
-	function changeColor(color) {
-		if (lastCell !== null)
-			fillCellWithColor(lastCell.cellX, lastCell.cellY, lastCell.color);
-		setSelectedColor(color.hex);
-	}
-	useEffect(() => {
-		const canvas = document.getElementById('boardCanvas');
-		const grid = document.getElementById('grid');
-		const gridCtx = grid.getContext('2d');
-		axios.get('http://localhost:3003/pixelBoards/' + params.id).then((res) => {
-			let board = res.data;
-			board.creation_date = new Date(board.creation_date).toLocaleDateString('fr-FR');
-			board.end_date = new Date(board.end_date).toLocaleDateString('fr-FR');
-			setBoard(board);
-			canvas.width = res.data.size * 20;
-			canvas.height = res.data.size * 20;
-			const ctx = canvas.getContext('2d');
-			ctx.fillStyle = '#ffffff';
-			ctx.fillRect(0, 0, canvas.width, canvas.height);
-			gridCtx.canvas.width = canvas.width;
-			gridCtx.canvas.height = canvas.height;
-			drawGrid(ctx, res.data.size * 20);
-			axios.get('http://localhost:3003/pixelboards/' + params.id + "/pixels").then((response) => {
-				setPixels(response.data);
-				response.data.forEach(pixel => {
-					fillCellWithColor(pixel.x, pixel.y, pixel.color);
-				});
-
-			});
-		})
-		// canvas.width = panelWidth;
-		// canvas.height = panelHeight;	
-		// ctx.fillStyle = '#ffffff';
-		// ctx.fillRect(0, 0, panelWidth, panelHeight);
-
-		// gridCtx.canvas.width = panelWidth;
-		// gridCtx.canvas.height = panelHeight;
-		// drawGrid(ctx);
-
-	}, []);
 	function drawGrid(ctx, size) {
-
 		for (let x = 0; x <= size; x += 20) {
 			for (let y = 0; y <= size; y += 20) {
 				ctx.moveTo(x, 0);
@@ -87,6 +48,47 @@ function BoardEditor(props) {
 		ctx.fillStyle = color;
 		ctx.fillRect((x * cellSize) + 1, (y * cellSize) + 1, cellSize - 2, cellSize - 2);
 	}
+	useEffect(() => {
+		const canvas = document.getElementById('boardCanvas');
+		const grid = document.getElementById('grid');
+		const gridCtx = grid.getContext('2d');
+		axios.get(`http://localhost:3003/pixelBoards/${params.id}`).then((res) => {
+			let pboard = res.data;
+			pboard.creation_date = new Date(pboard.creation_date).toLocaleDateString('fr-FR');
+			pboard.end_date = new Date(pboard.end_date).toLocaleDateString('fr-FR');
+			setBoard(pboard);
+			canvas.width = res.data.size * 20;
+			canvas.height = res.data.size * 20;
+			const ctx = canvas.getContext('2d');
+			ctx.fillStyle = '#ffffff';
+			ctx.fillRect(0, 0, canvas.width, canvas.height);
+			gridCtx.canvas.width = canvas.width;
+			gridCtx.canvas.height = canvas.height;
+			drawGrid(ctx, res.data.size * 20);
+			axios.get(`http://localhost:3003/pixelboards/${params.id}/pixels`).then((response) => {
+				setPixels(response.data);
+				response.data.forEach((pixel) => {
+					fillCellWithColor(pixel.x, pixel.y, pixel.color);
+				});
+			});
+		});
+		// canvas.width = panelWidth;
+		// canvas.height = panelHeight;
+		// ctx.fillStyle = '#ffffff';
+		// ctx.fillRect(0, 0, panelWidth, panelHeight);
+
+		// gridCtx.canvas.width = panelWidth;
+		// gridCtx.canvas.height = panelHeight;
+		// drawGrid(ctx);
+	}, []);
+	function setLastColor(cellX, cellY) {
+		for (let i = 0; i < pixels.length; i += 1) {
+			if (pixels[i].x === cellX && pixels[i].y === cellY) {
+				return pixels[i].color;
+			}
+		}
+		return '#ffffff';
+	}
 	function fillCell(cellX, cellY) {
 		const currentColor = selectedColor;
 		const canvas = document.getElementById('boardCanvas');
@@ -100,19 +102,9 @@ function BoardEditor(props) {
 		ctx.fillStyle = currentColor;
 		ctx.fillRect(startX + 1, startY + 1, cellSize - 2, cellSize - 2);
 		lastCell = { cellX, cellY, color: setLastColor(cellX, cellY) };
-		document.getElementById('cellX').textContent = "Cell X : " + cellX;
-		document.getElementById('cellY').textContent = "Cell Y : " + cellY;
-		document.getElementById('cellColor').textContent = "Cell Color : " + currentColor;
-
-	}
-	function setLastColor(cellX, cellY) {
-		for (let i = 0; i < pixels.length; i++) {
-			if (pixels[i].x == cellX && pixels[i].y == cellY) {
-				return pixels[i].color;
-			}
-		}
-
-		return '#ffffff';
+		document.getElementById('cellX').textContent = `Cell X : ${cellX}`;
+		document.getElementById('cellY').textContent = `Cell Y : ${cellY}`;
+		document.getElementById('cellColor').textContent = `Cell Color : ${currentColor}`;
 	}
 
 	function switchEditMode() {
@@ -133,7 +125,7 @@ function BoardEditor(props) {
 		const canvas = document.getElementById('boardCanvas');
 		const img = canvas.toDataURL('image/png');
 		if (currentCell === null) {
-			alert("Select a Pixel to update");
+			alert('Select a Pixel to update');
 			return;
 		}
 		axios(
@@ -146,96 +138,97 @@ function BoardEditor(props) {
 					x: currentCell.cellX,
 					y: currentCell.cellY,
 					author_id: sessionStorage.getItem('user_id'),
-					pixel_board_id: params.id.toString()
+					pixel_board_id: params.id.toString(),
 				},
-				withCredentials: true
-			}
+				withCredentials: true,
+			},
 		).then((res) => {
-			if (res.status == 200) {
-				axios.patch('http://localhost:3003/pixelboards/' + params.id + "/patch", {
-					thumbnail: img
+			if (res.status === 200) {
+				axios.patch(`http://localhost:3003/pixelboards/${params.id}/patch`, {
+					thumbnail: img,
 				});
-				alert("Pixel Updated");
-				setEditMode(false)
+				alert('Pixel Updated');
+				setEditMode(false);
 			}
 		}).catch((error) => {
-			alert("Cant draw yet ;)");
+			alert('Cant draw yet ;)');
 			window.location.reload();
-			setEditMode(false)
-
+			setEditMode(false);
 		});
 	}
+	function changeColor(color) {
+		if (lastCell !== null) { fillCellWithColor(lastCell.cellX, lastCell.cellY, lastCell.color); }
+		setSelectedColor(color.hex);
+	}
 	return (
-		<>
-			<div style={{
-				display: "flex",
-				justifyContent: "space-evenly",
-				alignItems: "center",
-				flexDirection: "row",
-			}}>
-				<div>
-					<h2>PixelBoard : {board.title}</h2>
-					<div style={{
-						display: "flex",
-						justifyContent: "center",
-						alignItems: "center",
-						flexDirection: "column",
-					}}>
-						<p className="fs-4">Creation date : <strong>{board.creation_date}</strong></p>
-						<p className="fs-4">End date : <strong>{board.end_date}</strong></p>
-						<p className="fs-4">Size : <strong>{board.size}</strong></p>
-						<p className="fs-4">Visitor allowed : <strong>{board.visitor_allowed ? "Yes" : "No"}</strong></p>
-						<p className="fs-4">Cooldown : <strong>{Math.floor(board.user_delay / 1000)} S</strong></p>
-						<p className="fs-4">Replace Pixel : <strong>{board.override_available ? "Yes" : "No"}</strong></p>
+		<div style={{
+			display: 'flex',
+			justifyContent: 'space-evenly',
+			alignItems: 'center',
+			flexDirection: 'row',
+		}}
+		>
+			<div>
+				<h2>PixelBoard : {board.title}</h2>
+				<div style={{
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+					flexDirection: 'column',
+				}}
+				>
+					<p className="fs-4">Creation date : <strong>{board.creation_date}</strong></p>
+					<p className="fs-4">End date : <strong>{board.end_date}</strong></p>
+					<p className="fs-4">Size : <strong>{board.size}</strong></p>
+					<p className="fs-4">Visitor allowed : <strong>{board.visitor_allowed ? 'Yes' : 'No'}</strong></p>
+					<p className="fs-4">Cooldown : <strong>{Math.floor(board.user_delay / 1000)} S</strong></p>
+					<p className="fs-4">Replace Pixel : <strong>{board.override_available ? 'Yes' : 'No'}</strong></p>
 
-					</div>
-				</div>
-				<div>
-					{editMode
-						? (
-							<>
-								<div className="boardEditor">
-									<p>&nbsp;</p>
-									<canvas id="grid" />
-									<canvas id="boardCanvas" onMouseDown={handleCanvasMousedown} ref={panelRef} />
-
-								</div>
-								<div className='pixelInfo'>
-									<label>Pick a color : &nbsp;</label>
-									<CirclePicker color={selectedColor} onChangeComplete={changeColor} id="colorPicker" />
-									<p id='cellX'></p>
-									<p id='cellY'></p>
-									<p id='cellColor'></p>
-									{sessionStorage.getItem("user_id") !== null ?
-										<button className="btn btn-success" type="button" onClick={updateBoard}>SAVE PIXEL</button> : <h2>Sign In to draw a pixel !</h2>}
-									<button className="btn btn-success" type="button" onClick={() => exportComponentAsPNG(panelRef)}>EXPORT TO PNG</button>
-
-
-								</div>
-
-							</>
-						)
-						: (
-							<>
-								<div className="boardEditor">
-									<p>&nbsp;</p>
-									<canvas id="grid" />
-									<canvas id="boardCanvas" ref={panelRef} />
-
-								</div>
-								<div className='pixelInfo'>
-									{sessionStorage.getItem("user_id") !== null ?
-										<button className="btn btn-success" style={{ margin: "15px" }} type="button" onClick={switchEditMode}>DRAW PIXEL</button> : <h2>Sign In to draw a pixel !</h2>}
-									<button className="btn btn-success" type="button" onClick={() => exportComponentAsPNG(panelRef)}>Export to PNG</button>
-								</div>
-
-							</>
-						)}
 				</div>
 			</div>
+			<div>
+				{editMode
+					? (
+						<>
+							<div className="boardEditor">
+								<p>&nbsp;</p>
+								<canvas id="grid" />
+								<canvas id="boardCanvas" onMouseDown={handleCanvasMousedown} ref={panelRef} />
 
-		</>
+							</div>
+							<div className="pixelInfo">
+								<label>Pick a color : &nbsp;</label>
+								<CirclePicker color={selectedColor} onChangeComplete={changeColor} id="colorPicker" />
+								<p id="cellX" />
+								<p id="cellY" />
+								<p id="cellColor" />
+								{sessionStorage.getItem('user_id') !== null
+									? <button className="btn btn-success" type="button" onClick={updateBoard}>SAVE PIXEL</button> : <h2>Sign In to draw a pixel !</h2>}
+								<button className="btn btn-success" type="button" onClick={() => exportComponentAsPNG(panelRef)}>EXPORT TO PNG</button>
+
+							</div>
+
+						</>
+					)
+					: (
+						<>
+							<div className="boardEditor">
+								<p>&nbsp;</p>
+								<canvas id="grid" />
+								<canvas id="boardCanvas" ref={panelRef} />
+
+							</div>
+							<div className="pixelInfo">
+								{sessionStorage.getItem('user_id') !== null
+									? <button className="btn btn-success" style={{ margin: '15px' }} type="button" onClick={switchEditMode}>DRAW PIXEL</button> : <h2>Sign In to draw a pixel !</h2>}
+								<button className="btn btn-success" type="button" onClick={() => exportComponentAsPNG(panelRef)}>Export to PNG</button>
+							</div>
+
+						</>
+					)}
+			</div>
+		</div>
 	);
-};
+}
 
 export default BoardEditor;
